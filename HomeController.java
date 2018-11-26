1a3d4446a7f50ac854de6a101cec5cf03dde6237
Olaoluwa3 <olaoko3@gmail.com>
2018-11-26 09:06:40 -0500

    Committing exercise 1.01

--- HomeController.java
+++ HomeController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class HomeController {
     @RequestMapping("/")
-    public String homePage(){ return "index";
+    public String homePage(){
+        return "index";
 
     }
 }
